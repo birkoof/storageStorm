26b2823cf06d4a7f4bf73ed5ef4e791861f52d8a
Eman Basic <eman.basic@student.tugraz.at>
2019-03-16 17:53:02 +0100

    fragments are shown and hidden, not destroyed

--- app/src/main/java/com/basic/storagestorm/MainActivity.kt
+++ app/src/main/java/com/basic/storagestorm/MainActivity.kt
@@ -1,56 +1,81 @@
 package com.basic.storagestorm
 
 import android.os.Bundle
+import android.provider.Settings
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : FragmentActivity() {
-
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_database -> {
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.fragmentContainer, DatabaseFragment.newInstance(), "databaseFragment")
-                    .addToBackStack(null)
+                    .hide(active)
+                    .show(databaseFragment)
                     .commit()
+
+                active = databaseFragment
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_settings -> {
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.fragmentContainer, SettingsFragment.newInstance(), "databaseFragment")
-                    .addToBackStack(null)
+                    .hide(active)
+                    .show(settingsFragment)
                     .commit()
+
+                active = settingsFragment
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_users -> {
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.fragmentContainer, UsersFragment.newInstance(), "databaseFragment")
-                    .addToBackStack(null)
+                    .hide(active)
+                    .show(usersFragment)
                     .commit()
+
+                active = usersFragment
+
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    private val databaseFragment = DatabaseFragment.newInstance()
+    private val usersFragment = UsersFragment.newInstance()
+    private val settingsFragment = SettingsFragment.newInstance()
+    var active: Fragment = databaseFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
         // display database fragment first on start up
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragmentContainer, DatabaseFragment.newInstance(), "databaseFragment")
-                .commit()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, usersFragment, "databaseFragment")
+            .hide(usersFragment)
+            .commit()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, settingsFragment, "databaseFragment")
+            .hide(settingsFragment)
+            .commit()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, databaseFragment, "databaseFragment")
+            .commit()
 
-            navigation.selectedItemId = R.id.navigation_database
-        }
+        active = databaseFragment
+        navigation.selectedItemId = R.id.navigation_database
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onAttachFragment(fragment: Fragment?) {
+        super.onAttachFragment(fragment)
     }
 }
