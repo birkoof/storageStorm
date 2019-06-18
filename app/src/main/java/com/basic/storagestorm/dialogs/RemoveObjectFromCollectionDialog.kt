83fda2ad058649fa9d24c263f04f7aa571eb4726
Eman Basic <eman.basic@student.tugraz.at>
2019-06-18 20:16:45 +0200

    implement remove object from collection

--- app/src/main/java/com/basic/storagestorm/dialogs/RemoveObjectFromCollectionDialog.kt
+++ app/src/main/java/com/basic/storagestorm/dialogs/RemoveObjectFromCollectionDialog.kt
@@ -2,7 +2,12 @@ package com.basic.storagestorm.dialogs
 
 import android.os.Bundle
 import android.widget.Toast
+import at.tugraz.ikarus.api.IkarusApi
+import com.basic.storagestorm.utilities.Constants
 import kotlinx.android.synthetic.main.activity_base_dialog.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.IOException
 
 class RemoveObjectFromCollectionDialog : BaseDialogActivity() {
 
@@ -15,10 +20,34 @@ class RemoveObjectFromCollectionDialog : BaseDialogActivity() {
 
         tvSave.text = "REMOVE"
         tvSave.setOnClickListener {
-            Toast.makeText(this, "TODO remove", Toast.LENGTH_SHORT).show()
+            executeRemove(firstInput.text.toString(), secondInput.text.toString())
         }
         tvCancel.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun executeRemove(collID: String, objID: String) {
+        doAsync {
+            try {
+                val success = IkarusApi(Constants.UTILITIES_SERVER_URL).removeColl(collID, objID)
+                uiThread {
+                    if (success) {
+                        Toast.makeText(it, "$objID deleted from $collID!", Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        Toast.makeText(it, "Please enter valid Collection/Object ID", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } catch (exception: IOException) {
+                uiThread {
+                    Toast.makeText(it, "Network error!", Toast.LENGTH_SHORT).show()
+                }
+            } catch (exception: NullPointerException) {
+                uiThread {
+                    Toast.makeText(it, "Please enter valid Collection/Object ID", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
