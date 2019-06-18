4902f2198aeabed9c58fd7adf94ece33f4c9ae80
Eman Basic <eman.basic@student.tugraz.at>
2019-06-18 20:12:05 +0200

    implemented add obj to coll

--- app/src/main/java/com/basic/storagestorm/dialogs/AddObjectToCollectionDialog.kt
+++ app/src/main/java/com/basic/storagestorm/dialogs/AddObjectToCollectionDialog.kt
@@ -2,7 +2,12 @@ package com.basic.storagestorm.dialogs
 
 import android.os.Bundle
 import android.widget.Toast
+import at.tugraz.ikarus.api.IkarusApi
+import com.basic.storagestorm.utilities.Constants
 import kotlinx.android.synthetic.main.activity_base_dialog.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.IOException
 
 class AddObjectToCollectionDialog : BaseDialogActivity() {
 
@@ -15,10 +20,34 @@ class AddObjectToCollectionDialog : BaseDialogActivity() {
 
         tvSave.text = "ADD"
         tvSave.setOnClickListener {
-            Toast.makeText(this, "TODO save", Toast.LENGTH_SHORT).show()
+            executeInsert(firstInput.text.toString(), secondInput.text.toString())
         }
         tvCancel.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun executeInsert(collID: String, objID: String) {
+        doAsync {
+            try {
+                val success = IkarusApi(Constants.UTILITIES_SERVER_URL).insertColl(collID, objID)
+                uiThread {
+                    if (success) {
+                        Toast.makeText(it, "$objID inserted into $collID!", Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        Toast.makeText(it, "Please enter valid Collection/Object ID", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } catch (exception: IOException) {
+                uiThread {
+                    Toast.makeText(it, "Network error!", Toast.LENGTH_SHORT).show()
+                }
+            } catch (exception: NullPointerException) {
+                uiThread {
+                    Toast.makeText(it, "Please enter valid Collection/Object ID", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
