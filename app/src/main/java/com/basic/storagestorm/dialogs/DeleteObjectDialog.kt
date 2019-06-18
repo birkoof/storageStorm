d89ecfaba1ef9f285324df5aaf1616a8a503c1f5
Eman Basic <eman.basic@student.tugraz.at>
2019-06-18 19:59:46 +0200

    implemented delete

--- app/src/main/java/com/basic/storagestorm/dialogs/DeleteObjectDialog.kt
+++ app/src/main/java/com/basic/storagestorm/dialogs/DeleteObjectDialog.kt
@@ -3,7 +3,12 @@ package com.basic.storagestorm.dialogs
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import at.tugraz.ikarus.api.IkarusApi
+import com.basic.storagestorm.utilities.Constants
 import kotlinx.android.synthetic.main.activity_base_dialog.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.IOException
 
 class DeleteObjectDialog : BaseDialogActivity() {
 
@@ -15,10 +20,34 @@ class DeleteObjectDialog : BaseDialogActivity() {
         secondInput.visibility = View.GONE
         tvSave.text = "DELETE"
         tvSave.setOnClickListener {
-            Toast.makeText(this, "TODO delete", Toast.LENGTH_SHORT).show()
+            executeDelete(firstInput.text.toString())
         }
         tvCancel.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun executeDelete(objectID: String) {
+        doAsync {
+            try {
+                IkarusApi(Constants.UTILITIES_SERVER_URL).delete(objectID)
+                uiThread {
+                    Toast.makeText(it, "$objectID deleted!", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            } catch (exception: IOException) {
+                uiThread {
+                    Toast.makeText(it, "Network error!", Toast.LENGTH_SHORT).show()
+                }
+            } catch (exception: IllegalStateException) {
+                uiThread {
+                    Toast.makeText(it, "Please enter valid Object ID", Toast.LENGTH_SHORT).show()
+                }
+            } catch (exception: NullPointerException) {
+                uiThread {
+                    Toast.makeText(it, "Please enter valid Object ID", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
